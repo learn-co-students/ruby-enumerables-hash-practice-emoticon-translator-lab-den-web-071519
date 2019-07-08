d3fa697e7ba28292b2811a61e05d43aa61b05b04
Tyler Masterson <tylercmasterson@gmail.com>
2019-07-08 20:34:15 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,23 @@
 # require modules here
+require "yaml"
+require "pry"
 
-def load_library
-  # code goes here
+def load_library(yml)
+  emo = YAML.load_file('lib/emoticons.yml')
+  list = {}
+  emo.each do |meaning, emoti|
+    if !list['get_meaning']
+    list['get_meaning'] = {}
+    end
+    if !list['get_emoticon']
+    list['get_emoticon'] = {}
+    end
+    emoti.each do |em|
+    list['get_meaning'][em] = meaning
+    list['get_emoticon'][emoti[0]] = emoti[1]
+    end
+  end
+  list
 end
 
 def get_japanese_emoticon
