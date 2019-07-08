2c495329de5515477eda7469feaa1515af2f5a2c
Ethan Collins <ethantcollins98@gmail.com>
2019-07-08 21:24:38 +0000

    load_libary method working

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,18 @@
-# require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(filePath)
+  hash = YAML.load_file(filePath)
+  hashCopy = {'get_meaning' => {}, 'get_emoticon' => {}}
+  hash.each do |key, value|
+    hashCopy['get_meaning'][value[1]] = key
+    hashCopy['get_emoticon'][value[0]] = value[1]
+=begin
+    puts key
+    puts value[0]
+    puts value[1]
+=end
+  end
+  hashCopy
 end
 
 def get_japanese_emoticon
